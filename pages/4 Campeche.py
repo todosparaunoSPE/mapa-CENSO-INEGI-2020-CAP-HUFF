ec0d64910dc7d76f3f7e07b99cd10e1b11d32242
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-02-16 10:25:28 -0600

    Update 4 Campeche.py

--- pages/4 Campeche.py
+++ pages/4 Campeche.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.link_button("Campeche-mapa", "https://2z6f2lrzc5fegrmyunpt2q.on.drv.tw/html/campeche.html")
+st.write("Campeche-Mapa [link](https://2z6f2lrzc5fegrmyunpt2q.on.drv.tw/html/Campeche-CAP.html)")
 
 
 
