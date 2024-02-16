f009f67ecb9d696dacc3fc22ff7da32e547a9e31
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-02-16 10:17:54 -0600

    Update 9 Colima.py

--- pages/9 Colima.py
+++ pages/9 Colima.py
@@ -9,6 +9,7 @@ import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
+import webbrowser
 
 
 
