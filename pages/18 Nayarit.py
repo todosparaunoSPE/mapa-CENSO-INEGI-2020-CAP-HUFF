7f8b72480979a5313375a52ff816ee81f0650f94
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-04 11:50:51 -0600

    Update 18 Nayarit.py

--- pages/18 Nayarit.py
+++ pages/18 Nayarit.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Nayarit-Mapa [link](https://2z6f2lrzc5fegrmyunpt2q.on.drv.tw/html/Nayarit-CAP.html)")
+st.write("Nayarit-Mapa [link](https://todosparaunospe.github.io/Nayarit/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
