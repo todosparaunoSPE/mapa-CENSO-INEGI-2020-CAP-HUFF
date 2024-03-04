700882c1c86064d2c320ba0cc8f0bd1f3e269c3e
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-04 11:21:09 -0600

    Update 8 Coahuila.py

--- pages/8 Coahuila.py
+++ pages/8 Coahuila.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Coahuila-Mapa [link](https://2z6f2lrzc5fegrmyunpt2q.on.drv.tw/html/Coahuila-CAP.html)")
+st.write("Coahuila-Mapa [link](https://todosparaunospe.github.io/Coahuila/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
