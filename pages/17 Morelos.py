89552f5f3aa6dd49e38f235029bb082fe853017c
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-06 17:56:31 -0600

    Update 17 Morelos.py

--- pages/17 Morelos.py
+++ pages/17 Morelos.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Morelos-Mapa [link](https://todosparaunospe.github.io/Morelos/)")
+st.write("Morelos-Mapa [link](https://todosparaunospe.github.io/Morelos-17/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
