a44aae32d62eb9b59ae7cf0d91649d472423bab1
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-06 17:26:25 -0600

    Update 8 Coahuila.py

--- pages/8 Coahuila.py
+++ pages/8 Coahuila.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Coahuila-Mapa [link](https://todosparaunospe.github.io/entidad-8/)")
+st.write("Coahuila-Mapa [link](https://todosparaunospe.github.io/Coahuila-VII/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
