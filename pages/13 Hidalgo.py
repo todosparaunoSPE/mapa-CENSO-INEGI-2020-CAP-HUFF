c3ed5fcaa296bb8ac0e199f75665a910b2265ca4
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-06 17:45:58 -0600

    Update 13 Hidalgo.py

--- pages/13 Hidalgo.py
+++ pages/13 Hidalgo.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Hidalgo-Mapa [link](https://todosparaunospe.github.io/Hidalgo/)")
+st.write("Hidalgo-Mapa [link](https://todosparaunospe.github.io/Hidalgo-13/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
