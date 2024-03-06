99f14fdc1a9ba34cc64b5aca2cb3c5fb4508bc6d
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-06 17:02:46 -0600

    Update 3 Baja California Sur.py

--- pages/3 Baja California Sur.py
+++ pages/3 Baja California Sur.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Baja California Sur-Mapa [link](https://todosparaunospe.github.io/entidad-3/)")
+st.write("Baja California Sur-Mapa [link](https://todosparaunospe.github.io/Baja-California-Sur-3/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
