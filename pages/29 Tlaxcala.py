22b6d8f034518cfc27c7653c7c0e3790f9c9ab85
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-06 18:27:50 -0600

    Update 29 Tlaxcala.py

--- pages/29 Tlaxcala.py
+++ pages/29 Tlaxcala.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Tlaxcala-Mapa [link](https://todosparaunospe.github.io/Tlaxcala/)")
+st.write("Tlaxcala-Mapa [link](https://todosparaunospe.github.io/Tlaxcala-29/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
