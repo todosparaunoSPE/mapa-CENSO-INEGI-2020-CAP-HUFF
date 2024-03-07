2856595fcd9775748447ea77383083f430cc153b
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-06 18:30:02 -0600

    Update 30 Veracruz.py

--- pages/30 Veracruz.py
+++ pages/30 Veracruz.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Veracruz-Mapa [link](https://todosparaunospe.github.io/Veracruz/)")
+st.write("Veracruz-Mapa [link](https://todosparaunospe.github.io/Veracruz-30/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
