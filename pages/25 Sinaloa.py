0235f64e22046166d93888d05cb790333141c518
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-06 18:17:35 -0600

    Update 25 Sinaloa.py

--- pages/25 Sinaloa.py
+++ pages/25 Sinaloa.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Sinaloa-Mapa [link](https://todosparaunospe.github.io/Sinaloa/)")
+st.write("Sinaloa-Mapa [link](https://todosparaunospe.github.io/Sinaloa-25/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
