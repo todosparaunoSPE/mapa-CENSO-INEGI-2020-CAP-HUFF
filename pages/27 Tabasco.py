d772b297ad40713ecb7167028efcc4e716eddc68
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-06 18:22:19 -0600

    Update 27 Tabasco.py

--- pages/27 Tabasco.py
+++ pages/27 Tabasco.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Tabasco-Mapa [link](https://todosparaunospe.github.io/Tabasco/)")
+st.write("Tabasco-Mapa [link](https://todosparaunospe.github.io/Tabasco-27/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
