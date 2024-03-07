a8549d772daf181433de59baef11a6b2aadcf59f
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-06 18:33:36 -0600

    Update 31 Yucatán.py

--- pages/31 Yucatán.py
+++ pages/31 Yucatán.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Yucatán-Mapa [link](https://todosparaunospe.github.io/Yucatan/)")
+st.write("Yucatán-Mapa [link](https://todosparaunospe.github.io/Yucatan-31/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
