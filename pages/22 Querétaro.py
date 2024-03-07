46f472723a71a796f8c8875488a6be0f3f72af9f
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-06 18:09:51 -0600

    Update 22 Querétaro.py

--- pages/22 Querétaro.py
+++ pages/22 Querétaro.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Querétaro-Mapa [link](https://todosparaunospe.github.io/Queretaro/)")
+st.write("Querétaro-Mapa [link](https://todosparaunospe.github.io/Queretaro-22/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
