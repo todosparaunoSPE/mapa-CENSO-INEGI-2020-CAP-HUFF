f87e024abd1e5b54bc0ec6ba89295f300b51ac98
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-12 17:53:18 -0600

    Update 16 Michoacán.py

--- pages/16 Michoacán.py
+++ pages/16 Michoacán.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Michoacán-Mapa [link](https://todosparaunospe.github.io/Michoacan-16/)")
+st.write("Michoacán-Mapa [link](https://todosparaunospe.github.io/Michoacan-XVI/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
