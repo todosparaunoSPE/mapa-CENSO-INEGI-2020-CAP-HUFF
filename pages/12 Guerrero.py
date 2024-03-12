8b336400ac7fc4e047546a6c446e6a3434542adf
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-12 17:34:49 -0600

    Update 12 Guerrero.py

--- pages/12 Guerrero.py
+++ pages/12 Guerrero.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Guerrero-Mapa [link](https://todosparaunospe.github.io/Guerrero-12/)")
+st.write("Guerrero-Mapa [link](https://todosparaunospe.github.io/Guerrero-XII/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
