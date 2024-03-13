7a18dc0dde248981d3aaf94ef33ffbc175a749e6
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-12 18:53:51 -0600

    Update 19 Nuevo León.py

--- pages/19 Nuevo León.py
+++ pages/19 Nuevo León.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Nuevo León-Mapa [link](https://todosparaunospe.github.io/Nuevo-Leon-19/)")
+st.write("Nuevo León-Mapa [link](https://todosparaunospe.github.io/Nuevo-Leon-XIX/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
