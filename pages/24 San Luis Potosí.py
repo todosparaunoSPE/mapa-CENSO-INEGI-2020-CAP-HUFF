71a7360010a940b14b349a8579deec5c7ca9b3ec
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-12 18:13:21 -0600

    Update 24 San Luis Potosí.py

--- pages/24 San Luis Potosí.py
+++ pages/24 San Luis Potosí.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("San Luis Potosí-Mapa [link](https://todosparaunospe.github.io/San-Luis-Potosi-24/)")
+st.write("San Luis Potosí-Mapa [link](https://todosparaunospe.github.io/San-Luis-Potosi-XIV/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
