e5b073c6708f08b1201da44f4469252c9b3d7433
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-12 18:59:28 -0600

    Update 28 Tamaulipas.py

--- pages/28 Tamaulipas.py
+++ pages/28 Tamaulipas.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Tamaulipas-Mapa [link](https://todosparaunospe.github.io/Tamaulipas-28/)")
+st.write("Tamaulipas-Mapa [link](https://todosparaunospe.github.io/Tamaulipas-XVIII/)")
 
 
 
