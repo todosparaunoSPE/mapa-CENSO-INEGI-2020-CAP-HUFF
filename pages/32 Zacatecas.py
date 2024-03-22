f162d85837abb19dcb0b4d16fb072a3331c78dfb
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-22 12:37:36 -0600

    Update 32 Zacatecas.py

--- pages/32 Zacatecas.py
+++ pages/32 Zacatecas.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Zacatecas-Mapa [link](https://todosparaunospe.github.io/Zacatecas-XXXII/)")
+st.write("Zacatecas-Mapa [link](https://todosparaunospe.github.io/Zacatecas/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
