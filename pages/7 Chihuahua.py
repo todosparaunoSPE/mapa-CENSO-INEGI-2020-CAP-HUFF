58fa72556afc346955c050841c38ce9de2bf40e7
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-22 11:32:43 -0600

    Update 7 Chihuahua.py

--- pages/7 Chihuahua.py
+++ pages/7 Chihuahua.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.write("Chihuahua-Mapa [link](https://todosparaunospe.github.io/Chihuahua-7/)")
+st.write("Chihuahua-Mapa [link](https://todosparaunospe.github.io/Chihuahua/)")
 
 
 # ---- HIDE STREAMLIT STYLE ----
