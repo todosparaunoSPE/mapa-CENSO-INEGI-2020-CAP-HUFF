9729696cdbadaf2070c38d68824f89a16a5bcc41
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-03-25 12:21:51 -0600

    Update 0.- CAPS de PENSIONISSSTE.py

--- pages/0.- CAPS de PENSIONISSSTE.py
+++ pages/0.- CAPS de PENSIONISSSTE.py
@@ -60,7 +60,7 @@ st.subheader(" ")
 st.subheader("")
 st.subheader("")
 
-st.write("57 CAP's-Mapa [link](https://todosparaunospe.github.io/total-de-CAPS-57/)")
+st.write("57 CAP's-Mapa [link](https://todosparaunospe.github.io/57-total/)")
 
 
 st.sidebar.success("Seleccione una página de arriba.")
